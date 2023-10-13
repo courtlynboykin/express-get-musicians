a9c3ffe296577dff351af268f60b226299b6c6d1
courtlynboykin <courtlynb@google.com>
2023-10-13 12:45:17 -0400

    first commit

--- index.test.js
+++ index.test.js
@@ -11,12 +11,17 @@ const seedMusician = require("./seedData");
 
 
 describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+    // testing musicians endpoint
+    test("Testing musicians endpoint", async () => {
+        // Sends request to `/musicians` endpoint
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+    })
 
-
-
-
-    
+    test("Testing musicians JSON", async () => {
+        const result = await request(app).get("/musicians");
+        const resultData = JSON.parse(result.text)
+        const name = resultData[1].name
+        expect(name).toBe("Drake")
+    })
 })
